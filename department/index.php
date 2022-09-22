c5855070cfa796e3c7f8379d0db9115f3872b8cd
ahmedhussein74 <ahmedhusseinofficial22@gmail.com>
2022-09-22 09:54:17 +0200

    commit

--- department/index.php
+++ department/index.php
@@ -16,7 +16,7 @@ if (isset($_GET['delete'])) {
       window.location.replace("/odc/S6/department/index.php");
   </script>
   EOT;
-  echo($myJS);
+  echo ($myJS);
 }
 
 ?>
@@ -29,16 +29,16 @@ if (isset($_GET['delete'])) {
       <th>Actions</th>
     </tr>
     <?php foreach ($dpartment as $data) { ?>
-    <tr>
-      <td><?= $data['id'] ?></td>
-      <td><?= $data['name'] ?></td>
-      <td class='d-flex justify-content-around'>
-        <a class="btn btn-primary" href="/odc/S6/department/update.php?edit=<?= $data['id'] ?>">Update</a>
-        <a class="btn btn-danger" href="/odc/S6/department/index.php?delete=<?= $data['id'] ?>">Delete</a>
-      </td>
-    </tr>
+      <tr>
+        <td><?= $data['id'] ?></td>
+        <td><?= $data['name'] ?></td>
+        <td class='d-flex justify-content-around'>
+          <a class="btn btn-primary" href="/odc/S6/department/update.php?edit=<?= $data['id'] ?>">Update</a>
+          <a class="btn btn-danger" href="/odc/S6/department/index.php?delete=<?= $data['id'] ?>">Delete</a>
+        </td>
+      </tr>
     <?php } ?>
   </table>
 </div>
 
-<?php include '../shared/footer.php';?>
+<?php include '../shared/footer.php'; ?>
